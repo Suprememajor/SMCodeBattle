7f1bbe35606206803c6a056aa451019821cd1f1e
Suprememajor <ndipnobertetta@gmail.com>
2021-11-10 03:14:52 +0100

    added lombok

--- src/main/java/smcodebattle/Main.java
+++ src/main/java/smcodebattle/Main.java
@@ -4,7 +4,8 @@ package smcodebattle;
  * Created by suprememajor on the 11/10/21
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
