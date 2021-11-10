aecee05cc9757a60b404d70a554e0ab23c55dfc0
Suprememajor <ndipnobertetta@gmail.com>
2021-11-10 02:55:06 +0100

    project init

---
+++ src/main/java/smcodebattle/Main.java
@@ -0,0 +1,10 @@
+package smcodebattle;
+
+/**
+ * Created by suprememajor on the 11/10/21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
